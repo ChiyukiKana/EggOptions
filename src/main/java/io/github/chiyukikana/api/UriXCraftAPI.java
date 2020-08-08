536e2d0925c11fcbe0c7bfe2a55686c72ce2243a
Kadreix <63837629+kadreix@users.noreply.github.com>
2020-08-08 17:50:20 +0800

    code

---
+++ src/main/java/io/github/chiyukikana/api/UriXCraftAPI.java
@@ -0,0 +1,11 @@
+package io.github.chiyukikana.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UriXCraftAPI extends JavaPlugin {
+    public static String getPluginVersion = "1.1.3f";
+
+    public static String getPluginAuthor = "ChiyukiKana";
+
+    public static String getPluginName = "EggOptions";
+}
